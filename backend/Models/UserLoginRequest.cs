7a84220ebcbce6ac1dd9fc162d32c0bb5b5be8bf
Rafael santos <105682230+rafael-santos201@users.noreply.github.com>
2025-07-24 21:06:46 -0300

    Delete backend directory

--- backend/Models/UserLoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MeuSistemaApi.Models
-{
-    public class UserLoginRequest
-    {   
-        public required string Login { get; set; }
-        public required string Senha { get; set; }  
-    }
-}
