f4df71c2ce8b38cb205652d3be0d4fc57a3367be
Rafael santos <105682230+rafael-santos201@users.noreply.github.com>
2025-07-24 21:54:05 -0300

    .

---
+++ frontend/src/assets/PainelAdm.jsx
@@ -0,0 +1,10 @@
+
+
+function PainelAdm() {
+  return (
+    <div>
+      <h1>Painel de Administração</h1>
+      {/* Aqui você pode adicionar mais componentes ou lógica para o painel de administração */}
+    </div>
+  );
+}
